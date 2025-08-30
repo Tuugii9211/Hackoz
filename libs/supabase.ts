6e49b4d454c3ac0cdb510db7d05ef11500bff223
Tengis Buyanbaatar <tbuy0214@uni.sydney.edu.au>
2025-08-31 08:00:18 +1000

    fix: database key change

--- libs/supabase.ts
+++ libs/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
 const SUPABASE_URL = 'https://vqwzzrrmuqffgvtaguoi.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxd3p6cnJtdXFmZmd2dGFndW9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NDcyNzMsImV4cCI6MjA3MjEyMzI3M30.L9C9BPTliomT-NjGxsEO-o5uw8lyOhZfHsdMYA3_3Jg'; // safe to ship in client (with RLS!)
+const SUPABASE_ANON_KEY = 'sb_publishable_dqpaYhAZrRW-aAgGyf2U6g_hBonQIWj';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
